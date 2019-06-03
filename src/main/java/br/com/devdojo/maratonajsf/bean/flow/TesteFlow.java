733917e90197dcad31f5e682cf3acb20032af18a
Daniel Borges <daniel.lemes.borges@hmail.com>
2019-06-03 20:50:22 -0300

    Adicionando validação no backend

--- src/main/java/br/com/devdojo/maratonajsf/bean/flow/TesteFlow.java
+++ src/main/java/br/com/devdojo/maratonajsf/bean/flow/TesteFlow.java
@@ -1,8 +1,12 @@
 package br.com.devdojo.maratonajsf.bean.flow;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.flow.FlowScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named
 @FlowScoped(value="registration")
@@ -17,7 +21,25 @@ public class TesteFlow implements Serializable {
         System.out.println("Salvando no banco");
         System.out.println(nome);
         System.out.println(endereco);
-//        return "exitToInicio";
+        validarUsuario();
+
+    }
+
+    public String validarUsuario() {
+        List<String> listaErros = new ArrayList<String>();
+        if(this.nome.length()<5) {
+            listaErros.add("Nome de usário tem ter no mínimo 5 caracteres");
+        }
+        if(this.sobrenome.length()<5) {
+            listaErros.add("Sobrenome de usário tem ter no mínimo 5 caracteres");
+        }
+        if(listaErros.size() > 0) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    listaErros.toString(), "mensagem"));
+
+        }
+        return "exitToInicio";
     }
 
     public String getNome() {
