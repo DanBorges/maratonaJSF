138161a9517cacf3b9964d2ccefcab9ecc5a5c58
Daniel Borges <daniel.lemes.borges@hmail.com>
2019-06-03 21:37:00 -0300

    adicionando dependent scope

--- src/main/java/br/com/devdojo/maratonajsf/bean/dependent/TesteDependent.java
+++ src/main/java/br/com/devdojo/maratonajsf/bean/dependent/TesteDependent.java
@@ -1,12 +1,7 @@
-package br.com.devdojo.maratonajsf.bean.view;
-
-import br.com.devdojo.maratonajsf.bean.dependent.TesteDependent;
+package br.com.devdojo.maratonajsf.bean.dependent;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -14,30 +9,23 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Named
-@ViewScoped
-public class TesteView implements Serializable {
+@Dependent
+public class TesteDependent implements Serializable {
     private List<String> personagens = new ArrayList<String>();
     private List<String>personagemSelecionado = new ArrayList<String>();
-    private final TesteDependent dependent;
-
-    @Inject
-    public TesteView(TesteDependent dependent) {
-        this.dependent = dependent;
-    }
 
     @PostConstruct
     public void init() {
-        personagens.add("Xikamaru");
-        personagens.add("Sakura");
-        personagens.add("Inno");
-        personagens.add("Tsunabi");
+        personagens.add("Pica Pau");
+        personagens.add("Leôncio");
+        personagens.add("Zeca Urubu");
+        personagens.add("Zé jacaré");
     }
 
     public void selecionarPersonagem() {
         int index = ThreadLocalRandom.current().nextInt(4);
         String personagem = personagens.get(index);
         personagemSelecionado.add(personagem);
-        dependent.getPersonagemSelecionado().add(personagem);
 
     }
 
@@ -50,15 +38,4 @@ public class TesteView implements Serializable {
         this.personagemSelecionado = personagemSelecionado;
     }
 
-    public List<String> getPersonagens() {
-        return personagens;
-    }
-
-    public void setPersonagens(List<String> personagens) {
-        this.personagens = personagens;
-    }
-
-    public TesteDependent getDependent() {
-        return dependent;
-    }
 }
